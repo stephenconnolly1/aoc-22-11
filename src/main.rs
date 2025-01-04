fe3aa292bcbc266ef64e4c8753d25e836d83d928
Stephen Connolly <stephen@pedanticism.net>
2025-01-04 00:03:17 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 struct Monkey {
     items: Vec<i64>,
     op: fn(i64) -> i64, 
-    test: fn(i64) -> bool,
+    modulo: i64,
     to_if_true: usize,
     to_if_false: usize,
     num_inspections: i64,
@@ -19,42 +19,66 @@ struct InspectResult {
 }
 
 impl Monkey {
-    fn inspect (&mut self) -> Vec<InspectResult> {
+    fn inspect (&mut self, modulo_product: i64) -> Vec<InspectResult> {
         let mut results: Vec<InspectResult> = Vec::new();
         for i in self.items.iter() {
-            println!("Inspecting: {}", i);
             let mut new = (self.op)(*i);
-            println!("new value: {}", new);
-            new = new / 3;
-            println!("new value / 3: {}", new);
-            let test_result = (self.test)(new);
-            println!("test_result: {}", test_result);
-            let target_monkey = if test_result {&self.to_if_true} else {&self.to_if_false};
-            println!("throw {} to monkey {}", new, target_monkey) ;
-            println!("----");
+            new = new % modulo_product; 
+            let target_monkey = if new % self.modulo == 0 {&self.to_if_true} else {&self.to_if_false};
             results.push(InspectResult{
                 worry_level: new,
                 target_monkey: *target_monkey,
             });
-            self.num_inspections += 1;
-    
+            self.num_inspections += 1;    
         }
         self.items.clear();
         return results;
     }
 }
 
+fn process_monkeys(mut monkeys: Vec<Monkey>) {
+    let mut modulo_product:i64 = 1;
+    for i in monkeys.iter() { modulo_product = modulo_product * i.modulo; }
+    println! ("Modulo: {}", modulo_product);
+
+    let mut monkey=0;
+    for round in 1..10001 {
+        for i in 0..monkeys.len() {
+            //println!("Monkey{}", i);
+            let results = monkeys[i].inspect(modulo_product);
+            for r in results.iter() {
+                //println!("     Result: {:?}", r);
+                monkeys[r.target_monkey].items.push(r.worry_level);
+            }
+            monkey += 1;
+
+        }    
+        match round {
+            1|20|1000|2000|3000|4000|5000|6000|7000|8000|9000|10000 => {
+                println!("== After round {} ==", round);
+                for m in monkeys.iter() {
+                    println!("Monkey inspected items {} times. Values {:?} ", m.num_inspections, m.items);
+                }
+            }
+            _ => (),
+        }
+    }
+    let mut business: Vec<i64> =  monkeys.iter().map(|m| m.num_inspections).collect::<Vec<_>>();
+    business.sort_by(|a, b| b.cmp(a));
+    println!("business: {:?}", business[0] * business [1]);
+
+}
 #[cfg(test)]
 mod test1 {
     use super::*;
-    /* 
+
     #[test]
     fn t1() {
         let mut monkeys: Vec<Monkey> = Vec::new();
         monkeys.push(Monkey {
-            items: vec!(79,98),
+            items: vec!(79, 98),
             op: |a| a * 19,
-            test: |a| a % 23 == 0,
+            modulo: 23,
             to_if_true:2,
             to_if_false:3,
             num_inspections: 0,
@@ -63,16 +87,16 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(54, 65, 75, 74),
             op: |a| a + 6 ,
-            test: |a| a % 19 == 0,
+            modulo : 19,
             to_if_true:2,
             to_if_false:0,
             num_inspections: 0,
         });
 
         monkeys.push(Monkey {
-            items: vec!(79,60, 97),
+            items: vec!(79, 60, 97),
             op: |a| a * a,
-            test: |a| a % 13 == 0,
+            modulo: 13,
             to_if_true:1,
             to_if_false:3,
             num_inspections: 0,
@@ -81,40 +105,25 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(74),
             op: |a| a + 3,
-            test: |a| a % 17 == 0,
+            modulo: 17,
             to_if_true:0,
             to_if_false:1,
             num_inspections: 0,
         });
 
-        let mut monkey=0;
-        for round in 0..20 {
-            println!("Round {}", round);
-            for i in 0..monkeys.len() {
-                println!("Monkey{}", i);
-                let results = monkeys[i].inspect();
-                for r in results.iter() {
-                    println!("     Result: {:?}", r);
-                    monkeys[r.target_monkey].items.push(r.worry_level);
-                }
-                monkey += 1;
-            }    
+        process_monkeys(monkeys);
 
-        }
-        for m in monkeys.iter() {
-            println!("Items: {:?}, Inspections: {}", m.items, m.num_inspections);
-        }
         assert_eq!(true,true) ;
     }
-*/
-    ///////
+
+
     #[test]
-    fn t2() {
+     fn t2() {
         let mut monkeys: Vec<Monkey> = Vec::new();
         monkeys.push(Monkey {
             items: vec!(75, 75, 98, 97, 79, 97, 64),
             op: |a| a * 13,
-            test: |a| a % 19 == 0,
+            modulo: 19,
             to_if_true:2,
             to_if_false:7,
             num_inspections: 0,
@@ -123,7 +132,7 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(50, 99, 80, 84, 65, 95),
             op: |a| a + 2 ,
-            test: |a| a % 3 == 0,
+            modulo: 3,
             to_if_true: 4,
             to_if_false: 5,
             num_inspections: 0,
@@ -132,7 +141,7 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(96, 74, 68, 96, 56, 71, 75, 53),
             op: |a| a + 1,
-            test: |a| a % 11 == 0,
+            modulo: 11,
             to_if_true: 7,
             to_if_false: 3,
             num_inspections: 0,
@@ -141,17 +150,16 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(83, 96, 86, 58, 92),
             op: |a| a + 8,
-            test: |a| a % 17 == 0,
+            modulo: 17,
             to_if_true: 6,
             to_if_false: 1,
             num_inspections: 0,
         });
 
-
         monkeys.push(Monkey {
             items: vec!(99),
             op: |a| a * a,
-            test: |a| a % 5 == 0,
+            modulo: 5,
             to_if_true: 0,
             to_if_false: 5,
             num_inspections: 0,
@@ -159,7 +167,7 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(60, 54, 83),
             op: |a| a + 4,
-            test: |a| a % 2 == 0,
+            modulo: 2,
             to_if_true: 2,
             to_if_false: 0,
             num_inspections: 0,
@@ -167,7 +175,7 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(77, 67),
             op: |a| a * 17,
-            test: |a| a % 13 == 0,
+            modulo: 13,
             to_if_true: 4,
             to_if_false: 1,
             num_inspections: 0,
@@ -175,29 +183,12 @@ mod test1 {
         monkeys.push(Monkey {
             items: vec!(95, 65, 58, 76),
             op: |a| a + 5,
-            test: |a| a % 7 == 0,
+            modulo: 7,
             to_if_true: 3,
             to_if_false: 6,
             num_inspections: 0,
         });
-
-        let mut monkey=0;
-        for round in 0..20 {
-            println!("Round {}", round);
-            for i in 0..monkeys.len() {
-                println!("Monkey{}", i);
-                let results = monkeys[i].inspect();
-                for r in results.iter() {
-                    println!("     Result: {:?}", r);
-                    monkeys[r.target_monkey].items.push(r.worry_level);
-                }
-                monkey += 1;
-            }    
-
-        }
-        for m in monkeys.iter() {
-            println!("Items: {:?}, Inspections: {}", m.items, m.num_inspections);
-        }
+        process_monkeys(monkeys);
         assert_eq!(true,true) ;
     }
 
